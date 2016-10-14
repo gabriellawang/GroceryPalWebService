b357f4943243ebd1e4f2e71f70bd1873737fa765
Shuwei <gabriellawang@GabriellaWangs-MacBook-Pro.local>
2016-10-14 20:19:58 +0800

    table with dummy data
    3 tables: deal, vote, review

---
+++ sql/datas.sql
@@ -0,0 +1,119 @@
+CREATE DATABASE  IF NOT EXISTS `grocerypal` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `grocerypal`;
+-- MySQL dump 10.13  Distrib 5.6.19, for osx10.7 (i386)
+--
+-- Host: 127.0.0.1    Database: grocerypal
+-- ------------------------------------------------------
+-- Server version	5.6.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `deal`
+--
+
+DROP TABLE IF EXISTS `deal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `deal` (
+  `deal_id` int(11) NOT NULL,
+  `product_name` varchar(45) NOT NULL,
+  `brand_name` varchar(45) NOT NULL,
+  `price` double NOT NULL,
+  `shop` varchar(45) NOT NULL,
+  `location` varchar(45) NOT NULL,
+  `time` datetime NOT NULL,
+  `img_dir` varchar(80) NOT NULL,
+  `like_count` int(11) NOT NULL,
+  `dislike_count` int(11) NOT NULL,
+  `device_id` varchar(16) NOT NULL,
+  `api_keyword` varchar(60) NOT NULL,
+  PRIMARY KEY (`deal_id`),
+  UNIQUE KEY `deal_id_UNIQUE` (`deal_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `deal`
+--
+
+LOCK TABLES `deal` WRITE;
+/*!40000 ALTER TABLE `deal` DISABLE KEYS */;
+/*!40000 ALTER TABLE `deal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `review`
+--
+
+DROP TABLE IF EXISTS `review`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `review` (
+  `review_id` int(11) NOT NULL AUTO_INCREMENT,
+  `deal_id` int(11) NOT NULL,
+  `device_id` varchar(16) NOT NULL,
+  `review_time` datetime NOT NULL,
+  `content` varchar(200) NOT NULL,
+  PRIMARY KEY (`review_id`),
+  UNIQUE KEY `review_id_UNIQUE` (`review_id`),
+  KEY `deal_id_idx` (`deal_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `review`
+--
+
+LOCK TABLES `review` WRITE;
+/*!40000 ALTER TABLE `review` DISABLE KEYS */;
+INSERT INTO `review` VALUES (1,1,'1234567890123456','2016-10-14 19:37:00','good!');
+/*!40000 ALTER TABLE `review` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vote`
+--
+
+DROP TABLE IF EXISTS `vote`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vote` (
+  `vote_id` int(11) NOT NULL AUTO_INCREMENT,
+  `device_id` varchar(16) NOT NULL,
+  `is_like` tinyint(1) NOT NULL,
+  `deal_id` int(11) NOT NULL,
+  PRIMARY KEY (`vote_id`),
+  UNIQUE KEY `vote_id_UNIQUE` (`vote_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vote`
+--
+
+LOCK TABLES `vote` WRITE;
+/*!40000 ALTER TABLE `vote` DISABLE KEYS */;
+INSERT INTO `vote` VALUES (1,'1234567890123456',1,1);
+/*!40000 ALTER TABLE `vote` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-10-14 20:18:55
