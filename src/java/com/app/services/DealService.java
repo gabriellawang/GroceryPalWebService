733c11e52cca6b5f470d9578099ace31499b1484
Bowen <bowen.lyu.2014@smu.edu.sg>
2016-10-16 18:34:18 +0800

    Add some comments

--- src/java/com/app/services/DealService.java
+++ src/java/com/app/services/DealService.java
@@ -14,7 +14,10 @@ import org.json.JSONObject;
  * @author lvbowen
  */
 public class DealService {
-    public static String retrieveDeals(String jsonInput){
+    
+    //Given the String json input, this method searches either LOGO, Text, Label on Diffmarts
+    //Return the first result found in diffmarts, otherwise return a empty string.
+    public static String retrieveDiffMartsResult(String jsonInput){
         String diffMartsResult = "";
         
         try{
@@ -30,7 +33,9 @@ public class DealService {
             ArrayList<String> labels = new ArrayList<String>();
             ArrayList<String> texts = new ArrayList<String>();
             
-
+            //If the json input includes LOGO annotation and the score larger than 50,
+            //assigns the logo name to logoName variable, and hasValidLogo is ture.
+            //Otherwise, logoName variable is empty,and hasValidLogo is false;
             if(hasLogoAnnotations){
                 JSONArray jbArray_logoAnnotations = jbResponse.getJSONArray("logoAnnotations");
                 double score = jbArray_logoAnnotations.getJSONObject(0).getDouble("score");
@@ -40,7 +45,11 @@ public class DealService {
                 }
             }
 
+            
             if(!hasValidLogo){
+                //If logo is not found or invalid, and the json input includes Label annotation
+                //add labels, which score larger than 70, to labels arraylist.
+                //Otherwise, noResult is ture. 
                 if(hasLabelAnnotations){
                     JSONArray jbArray_labelAnnotations = jbResponse.getJSONArray("labelAnnotations"); 
                     for(int i = 0 ; i < jbArray_labelAnnotations.length() ; i++){
@@ -56,6 +65,9 @@ public class DealService {
                     noResult = true;
                 }
             } else {
+                //If the logo is found and valid, and the json input includes Text annotation
+                //add all texts to texts arraylist (there is no score for each text)
+                //Otherwise, noResult is true.
                 if(hasTextAnnotations){
                     JSONObject jb_textAnnotations = jbResponse.getJSONArray("textAnnotations").getJSONObject(0);
                     String textString = jb_textAnnotations.getString("description");
@@ -71,7 +83,7 @@ public class DealService {
             if(noResult){
 
             } else if(hasValidLogo){
-                //logo & text
+                // Search logo & text in diffMarts
                 String textStr = "";
                 for(String t : texts){
                     textStr = textStr + " " + t;
@@ -79,13 +91,14 @@ public class DealService {
                 diffMartsResult = DiffMartsCrawler.getFirstResultFor(logoName + textStr);
                 
             } else {
-                //label
+                // Search labels in diffMarts
                 String textStr = "";
                 for(String t : labels){
                     textStr += t + " ";
                 }
                 diffMartsResult = DiffMartsCrawler.getFirstResultFor(logoName + textStr.trim());
             }
+            
         } catch (Exception e){
             System.out.println(e.getMessage());
         }
@@ -93,8 +106,15 @@ public class DealService {
         return diffMartsResult;
     }
     
+    //Given diffmarts result, this method will retrieve relevant deals in local database
+    //Output is in Json format
+    public static String retrieveDeals(String diffMartsResult){
+        
+        return null;
+    }
+    
     public static void main(String [] args){
         String jsonInput = "{\"responses\":[{\"logoAnnotations\":[{\"mid\":\"/m/045c7b\",\"description\":\"pokka\",\"score\":0.85000956,\"boundingPoly\":{\"vertices\":[{\"x\":158,\"y\":50},{\"x\":515,\"y\":50},{\"x\":515,\"y\":156},{\"x\":158,\"y\":156}]}}],\"labelAnnotations\":[{\"mid\":\"/m/021sdg\",\"description\":\"pokka\",\"score\":0.87143095},{\"mid\":\"/m/0dgsmq8\",\"description\":\"artwork\",\"score\":0.86358012},{\"mid\":\"/m/0dwx7\",\"description\":\"logo\",\"score\":0.31318793},{\"mid\":\"/m/01mf0\",\"description\":\"software\",\"score\":0.23124418},{\"mid\":\"/m/03g09t\",\"description\":\"clip art\",\"score\":0.20368107},{\"mid\":\"/m/02ngh\",\"description\":\"emoticon\",\"score\":0.19831011},{\"mid\":\"/m/0h8npc5\",\"description\":\"digital content software\",\"score\":0.1769385},{\"mid\":\"/m/03tqj\",\"description\":\"icon\",\"score\":0.097528793},{\"mid\":\"/m/0hr95w1\",\"description\":\"pointer\",\"score\":0.03663468},{\"mid\":\"/m/0n0j\",\"description\":\"area\",\"score\":0.033584446}],\"textAnnotations\":[{\"locale\":\"en\",\"description\":\"Google\\n\",\"boundingPoly\":{\"vertices\":[{\"x\":61,\"y\":26},{\"x\":598,\"y\":26},{\"x\":598,\"y\":227},{\"x\":61,\"y\":227}]}}]}]}";
-        retrieveDeals(jsonInput);
+        retrieveDiffMartsResult(jsonInput);
     }
 }
